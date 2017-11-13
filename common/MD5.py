fc36c47e858b16a7f91532d36d2855915529a967
momo050200 <xhy886379@126.com>
2017-11-13 11:19:02 +0800

    zhangli

--- common/MD5.py
+++ common/MD5.py
@@ -1,12 +1,15 @@
 #coding=utf-8
 import hashlib,time
 
-X_Application_id = '12345678'
-app_id = 'test'
-date = '2017-11-13T10:59:12Z'
-print(time.ctime())
 
-m2 = hashlib.md5((X_Application_id+app_id+date).encode('utf-8'))
-print(m2)
-print(m2.hexdigest())
+def MD5():
+    X_Application_id = '12345678'
+    app_id = 'test'
+    date = '2017-11-13T10:59:12Z'
+
+    m2 = hashlib.md5((X_Application_id + app_id + date).encode('utf-8'))
+
+    return m2.hexdigest()
+
+
 
