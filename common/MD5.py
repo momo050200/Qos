82206a3c580159368d41a21536ea2e9da83433e2
xuhaiyan <17798502675@189.cn>
2017-11-13 16:31:29 +0800

    123

--- common/MD5.py
+++ common/MD5.py
@@ -7,7 +7,8 @@ def MD5():
     app_id = 'test'
     date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     m2 = hashlib.md5((X_Application_id + app_id + date).encode('utf-8'))
-    return date,m2.hexdigest()
+    X_Application_Auth = m2.hexdigest()
+    return X_Application_id,date,X_Application_Auth
 
 
 if __name__=='__main__':
