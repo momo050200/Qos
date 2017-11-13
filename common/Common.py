493ec7ef17c02ce6da0ae66273003e0956b9c148
xuhaiyan <17798502675@189.cn>
2017-11-13 16:07:34 +0800

    123

--- common/Common.py
+++ common/Common.py
@@ -5,17 +5,8 @@ from common.MD5 import MD5
 import requests
 import urllib.request
 
-def header(self,app_id,date,X_Application_Auth):
-    header = {
-        "X-Request-At": date,
-        "X-Application-Id": app_id,
-        "X_Application_Auth": X_Application_Auth,
-    }
-    return header
-
-def get_token(self, app_id,date,X_Application_Auth):
-    # date, X_Application_Auth = MD5()
-    url = "http://61.160.149.236:10000/qos-api/t1?APP_ID=" + app_id
+def get_token(self, X_Application_id,date,X_Application_Auth):
+    url = "http://61.160.149.236:10000/qos-api/t1?APP_ID=" + X_Application_id
     head = {
         "x-up-calling-line-id": "13390905442",
         "x-forwarded-for": "180.99.55.207",
@@ -24,31 +15,24 @@ def get_token(self, app_id,date,X_Application_Auth):
         "X-Rat-Type": "test",
         "X_Application_Auth": X_Application_Auth,
         "X-Request-At": date,
-        "X-Application-Id": app_id
+        "X-Application-Id": X_Application_id
     }
-    req = urllib.request.Request(url, headers=head)
-    response = urllib.request.urlopen(req)
-    compressedData = eval(response.read().decode())['result']
-    print(type(compressedData))
-    return compressedData
+    req = C.get(self,url,headers=head)
+    # req = urllib.request.Request(url, headers=head)
+    # response = urllib.request.urlopen(req)
+    # compressedData = eval(response.read().decode())['result']
+    # print(type(compressedData))
+    # return compressedData
+    return req
 
-def check(self,speed_id,date,X_Application_Auth,app_id,security_token):
+def check(self,speed_id,X_Application_id,date,X_Application_Auth):
     url = "http://61.160.149.236:10000/qos-api/speeding?speed_id=" + speed_id
     head = {
-        "x-up-calling-line-id": "13390905442",
-        "x-forwarded-for": "180.99.55.207",
-        "x-Called-Station-id": "test",
-        "x-User-Location-Info": "test",
-        "X-Rat-Type": "test",
-        "X-IMEI":"test",
         "X-Request-At": date,
-        "X-Application-Id": app_id,
+        "X-Application-Id": X_Application_id,
         "X_Application_Auth": X_Application_Auth,
-        "speed_id":speed_id,
-        "security_token":security_token,
-        "dst_info":"10.1.1.1",
-
     }
+    result = C.get(self,url,headers=head)
 
 
 
