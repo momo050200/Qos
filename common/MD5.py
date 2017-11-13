ee7ffbdd5ce34bbf1c8189e2562669052620990a
miaoxiangxiang <1327171279@qq.com>
2017-11-13 11:01:12 +0800

    缪香香

--- common/MD5.py
+++ common/MD5.py
@@ -1,6 +1,13 @@
+#coding=utf-8
+
 import hashlib
 
-X_Application_id = '12345678'
-app_id = 'test'
+def MD5(data):
 
-m2 = hashlib.md5()
+    m = hashlib.md5(data.encode(encoding='gb2312'))
+    Auth = m.hexdigest()
+    return Auth
+
+if __name__=='__main__':
+    data = '12345678test2017-11-09T01:46:00Z'
+    print(MD5(data))
