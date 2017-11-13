d26ef9ffdd25afcdb059546c55f55a7c7fb20370
xuhaiyan <17798502675@189.cn>
2017-11-13 17:09:09 +0800

    123

--- common/Common.py
+++ common/Common.py
@@ -1,8 +1,6 @@
 #coding=utf-8
 
 from common_interface import CommonInterface as C
-import requests
-
 
 def get_token(self, X_Application_id,date,X_Application_Auth):
     url = "http://61.160.149.236:10000/qos-api/t1?APP_ID=" + X_Application_id
@@ -16,13 +14,8 @@ def get_token(self, X_Application_id,date,X_Application_Auth):
         "X-Request-At": date,
         "X-Application-Id": X_Application_id
     }
-    req = requests.get(self,url,headers=head)
-    # req = urllib.request.Request(url, headers=head)
-    # response = urllib.request.urlopen(req)
-    # compressedData = eval(response.read().decode())['result']
-    # print(type(compressedData))
-    # return compressedData
-    return req
+    result = C.get(url,data=None,headers=head)
+    return result
 
 def check(self,speed_id,X_Application_id,date,X_Application_Auth):
     url = "http://61.160.149.236:10000/qos-api/speeding?speed_id=" + speed_id
