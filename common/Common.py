549fad70818e8397965eebc6cb3052a5c7d175b5
xuhaiyan <17798502675@189.cn>
2017-11-13 14:00:02 +0800

    123

---
+++ common/Common.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+
+from common_interface import CommonInterface as C
+from common.MD5 import MD5
+
+class Common():
+
+    def test_get_token(self,app_id):
+        date,X_Application_Auth = MD5()
+        url = "http://61.160.149.236:10000/qos-api/t1?APP_ID="+app_id
+        head = {
+            "x-up-calling-line-id":"13390905442",
+            "x-forwarded-for":"180.99.55.207",
+            "x-Called-Station-id":"test",
+            "x-User-Location-Info":"test",
+            "X-Rat-Type":"test",
+            "X_Application_Auth":X_Application_Auth,
+            "X-Request-At":date,
+            "X-Application-Id":app_id
+        }
+        result = C.get(self,url,data=head)
+        return result
+
+
+
+
+
--- common/MD5.py
+++ common/MD5.py
@@ -2,12 +2,12 @@
 import hashlib
 import datetime
 
-def MD5():
+def MD5(self):
     X_Application_id = '12345678'
     app_id = 'test'
     date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     m2 = hashlib.md5((X_Application_id + app_id + date).encode('utf-8'))
-    return m2.hexdigest()
+    return date,m2.hexdigest()
 
 
 if __name__=='__main__':
