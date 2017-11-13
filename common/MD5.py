a86aa322f581fcabfd9b36a6937e4ad60bf8ad8b
momo050200 <xhy886379@126.com>
2017-11-13 11:13:50 +0800

    zhangli

--- common/MD5.py
+++ common/MD5.py
@@ -1,6 +1,12 @@
-import hashlib
+#coding=utf-8
+import hashlib,time
 
 X_Application_id = '12345678'
 app_id = 'test'
+date = '2017-11-13T10:59:12Z'
+print(time.ctime())
 
-m2 = hashlib.md5()
+m2 = hashlib.md5((X_Application_id+app_id+date).encode('utf-8'))
+print(m2)
+print(m2.hexdigest())
+
