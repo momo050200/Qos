572b89944c24dc1079ea5a8b987a6908b39da663
xuhaiyan <17798502675@189.cn>
2017-11-14 10:51:58 +0800

    徐海燕

--- test/test.py
+++ test/test.py
@@ -58,6 +58,49 @@ class test(unittest.TestCase):
         result = {"code":"50000"}
         get_token(self,X_Application_id,date,X_Application_Auth,result)
 
+    def test_token_timeout(self):
+        '''提速时，token失效'''
+        X_Application_id = '12345678'
+        date, X_Application_Auth = MD5(X_Application_id)
+        # step1 获取token
+        token = get_token(self, X_Application_id, date, X_Application_Auth)['result']
+        print('step1 获取token：' + str(token))
+        # step2 申请提速
+        result2 = {'result': {'Done': 'True'}}
+        r = speeding(self, X_Application_id, date, X_Application_Auth, token, result2)
+        print('step2 申请提速:' + str(r))
+        speed_id = r['result']['speeed_id']
+        # step3 检查提速结果
+        result3 = {'msg': '成功', 'code': '0'}
+        c = check(self, speed_id, X_Application_id, date, X_Application_Auth, result3)
+        print('step3 检查提速结果：' + str(c))
+        # step4 撤销提速
+        result4 = {'result': {'Done': True, 'speeed_id': speed_id}}
+        d = delete_speeding(self, speed_id, X_Application_id, date, X_Application_Auth, result4)
+        print('step4 撤销提速：' + str(d))
+        # step5 检查撤销提速结果
+        result5 = {'msg': '对不起，系统异常', 'code': '10000'}
+        c2 = check(self, speed_id, X_Application_id, date, X_Application_Auth, result5)
+        print('step5 检查撤销提速结果：' + str(c2))
+        # step6 使用失效的token申请提速
+        result6 = {'code':'5065','msg':'资源申请失败'}
+        r = speeding(self, X_Application_id, date, X_Application_Auth, token, result6)
+        print('step6 使用失效的token申请提速:' + str(r))
+
+    def test_token_wrong(self):
+        '''提速时，token错误'''
+        X_Application_id = '12345678'
+        date, X_Application_Auth = MD5(X_Application_id)
+        # step1 获取token
+        token = get_token(self, X_Application_id, date, X_Application_Auth)['result']
+        print('step1 获取token：' + str(token))
+        # step2 使用错误的token申请提速
+        wrong_token=token+'123'
+        result2 = {'code':'5065','msg':'资源申请失败'}
+        r = speeding(self, X_Application_id, date, X_Application_Auth, wrong_token, result2)
+        print('step2 使用错误的token申请提速:' + str(r))
+
+
 
 
 
