facff6fb1544a67779269434702bbc32f429d460
momo050200 <xhy886379@126.com>
2017-11-17 14:56:22 +0800

    zhangli

--- common/Common.py
+++ common/Common.py
@@ -14,8 +14,8 @@ from common_interface import CommonInterface as C
 def get_token(self, X_Application_id,date,X_Application_Auth,results=None):
     url = "http://61.160.149.236:10000/qos-api/t1?APP_ID=12345678"
     head = {
-        "x-up-calling-line-id": "15366189548",
-        "x-forwarded-for": "10.165.11.138",
+        "x-up-calling-line-id": "17798502661",
+        "x-forwarded-for": "10.66.224.38",
         "x-Called-Station-id": "test",
         "x-User-Location-Info": "test",
         "X-Rat-Type": "test",
@@ -63,6 +63,33 @@ def delete_speeding(self,speed_id,X_Application_id,date,X_Application_Auth,resul
         "X-Application-Auth": X_Application_Auth,
     }
     result = C.delete(self,url,headers=head,results=results)
+    return result
+
+def add_product(self,results=None):
+    url = "http://61.160.149.236:10000/qos-api/addProduct"
+    body = {
+        'id':'101',
+        'location_code':'0',
+        'service_url':'http://192.168.203.65:18084/qos-bizcore',
+        'core_network_company':'0',
+        'load_type':'1',
+        'networking_type':'0',
+        'node_ip_port':'192.168.203.65:10002',
+        'node_hostname':'com.jshx.qos',
+        'remote_ip_port':'132.224.255.41:13868',
+        'remote_hostname':'pcrf009-b-ot.nj.js.node.epc.mnc011.mcc460.3gppnetwork.org',
+        'province_code':'js',
+        'delete_flag':'0',
+    }
+    result = C.delete(self,url,data=body,results=results)
+    return result
+
+def remove_product(self,product_key,results=None):
+    url = "http://61.160.149.236:10000/qos-api/removeProduct"
+    body = {
+        'key':product_key,
+    }
+    result = C.delete(self,url,data=body,results=results)
     return result
 
 
