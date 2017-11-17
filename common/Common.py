34fbdba95dedd3ec6cebf2daa3ce25045698019b
xuhaiyan <17798502675@189.cn>
2017-11-17 16:33:02 +0800

    徐海燕

--- common/Common.py
+++ common/Common.py
@@ -14,8 +14,10 @@ from common_interface import CommonInterface as C
 def get_token(self, X_Application_id,date,X_Application_Auth,results=None):
     url = "http://61.160.149.236:10000/qos-api/t1?APP_ID=12345678"
     head = {
-        "x-up-calling-line-id": "17798502661",
-        "x-forwarded-for": "10.66.224.38",
+        # "x-up-calling-line-id": "17798502661",
+        # "x-forwarded-for": "10.129.201.176",
+        "x-up-calling-line-id": "13390905442",
+        "x-forwarded-for": "49.90.4.96",
         "x-Called-Station-id": "test",
         "x-User-Location-Info": "test",
         "X-Rat-Type": "test",
@@ -36,8 +38,8 @@ def check(self,speed_id,X_Application_id,date,X_Application_Auth,results=None):
     result = C.get(self,url,headers=head,results=results)
     return result
 
-def speeding(self,X_Application_id,date,X_Application_Auth,security_token,url="http://61.160.149.236:10000/qos-api/speeding",results=None,dst_info="60.174.237.91:242"):
-    # url = "http://61.160.149.236:10000/qos-api/speeding"
+def speeding(self,X_Application_id,date,X_Application_Auth,security_token,results=None,dst_info="60.174.237.91:242"):
+    url = "http://61.160.149.236:10000/qos-api/speeding"
     head = {
         "X-Request-At": date,
         "X-Application-Id": X_Application_id,
@@ -56,7 +58,7 @@ def speeding(self,X_Application_id,date,X_Application_Auth,security_token,url="h
     return result
 
 def delete_speeding(self,speed_id,X_Application_id,date,X_Application_Auth,results=None):
-    url = "http://61.160.149.236:10000/qos-api/speeding?speeed_id=" + speed_id
+    url = "http://61.160.149.236:10000/qos-api/speeding?speeed_id=aar_" + speed_id
     head = {
         "X-Request-At": date,
         "X-Application-Id": X_Application_id,
@@ -65,7 +67,10 @@ def delete_speeding(self,speed_id,X_Application_id,date,X_Application_Auth,resul
     result = C.delete(self,url,headers=head,results=results)
     return result
 
-def add_product(self,results=None):
+def add_product(self,results=None,
+                node_ip_port='192.168.203.65:10002',
+                remote_ip_port='132.224.255.41:13868',
+                remote_hostname='pcrf009-b-ot.nj.js.node.epc.mnc011.mcc460.3gppnetwork.org'):
     url = "http://61.160.149.236:10000/qos-api/addProduct"
     body = {
         'id':'101',
@@ -74,14 +79,14 @@ def add_product(self,results=None):
         'core_network_company':'0',
         'load_type':'1',
         'networking_type':'0',
-        'node_ip_port':'192.168.203.65:10002',
+        'node_ip_port':node_ip_port,
         'node_hostname':'com.jshx.qos',
-        'remote_ip_port':'132.224.255.41:13868',
-        'remote_hostname':'pcrf009-b-ot.nj.js.node.epc.mnc011.mcc460.3gppnetwork.org',
+        'remote_ip_port':remote_ip_port,
+        'remote_hostname':remote_hostname,
         'province_code':'js',
         'delete_flag':'0',
     }
-    result = C.delete(self,url,data=body,results=results)
+    result = C.post(self,url,data=body,results=results)
     return result
 
 def remove_product(self,product_key,results=None):
@@ -89,7 +94,7 @@ def remove_product(self,product_key,results=None):
     body = {
         'key':product_key,
     }
-    result = C.delete(self,url,data=body,results=results)
+    result = C.post(self,url,data=body,results=results)
     return result
 
 
