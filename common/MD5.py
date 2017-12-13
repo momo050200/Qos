82bc3c5968cf67b3c6656fe7f05b8a754b08cc68
xuhaiyan <17798502675@189.cn>
2017-12-13 16:23:53 +0800

    徐海燕

--- common/MD5.py
+++ common/MD5.py
@@ -1,10 +1,13 @@
 #coding=utf-8
 import hashlib
 import datetime
+from IniReader import IniReader
+
 
 def MD5(X_Application_id):
+    file_path = '.\cfginfo.ini'
+    app_security = IniReader(file_path).get_value('MD5', 'app_security')
     # 定义需要进行加密的数据
-    app_security = 'test'
     # 生成UTC标准时间，格式：2017-11-14T01:02:00Z
     date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     # 对data进行md5加密
